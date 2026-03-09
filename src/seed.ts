7c17b9b28bba5bba671d5d8d6f6f09e4344f80a5
Athibet Bootklang <athibet.b@gmail.com>
2026-03-09 15:26:00 +0700

    add : missing cat product 😂

--- src/seed.ts
+++ src/seed.ts
@@ -6,25 +6,26 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import { Order } from './order/schemas/order.schema';
-// import { Category } from './product/schemas/category.schema';
+import { Category } from './product/schemas/category.schema';
+
 import { ORDER_TYPES } from './order/constants';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
 
   const orderModel = app.get<Model<Order>>(getModelToken(Order.name));
-  // const categoryModel = app.get<Model<Category>>(getModelToken(Category.name));
+  const categoryModel = app.get<Model<Category>>(getModelToken(Category.name));
 
   try {
-    // console.log('--- Start Import Master Data (Category) ---');
-    // const productFilePath = path.join(process.cwd(), 'util', 'Products.json');
-    // const productData = JSON.parse(fs.readFileSync(productFilePath, 'utf-8'));
+    console.log('--- Start Import Master Data (Category) ---');
+    const productFilePath = path.join(process.cwd(), 'util', 'Products.json');
+    const productData = JSON.parse(fs.readFileSync(productFilePath, 'utf-8'));
 
-    // await categoryModel.deleteMany({});
-    // await categoryModel.insertMany(productData.productList);
-    // console.log(
-    //   `✅ Import Category success: ${productData.productList.length} categories`,
-    // );
+    await categoryModel.deleteMany({});
+    await categoryModel.insertMany(productData.productList);
+    console.log(
+      `✅ Import Category success: ${productData.productList.length} categories`,
+    );
 
     console.log('\n--- Start Import Transaction (Order) ---');
     const orderFilePath = path.join(process.cwd(), 'util', 'Orders.json');
